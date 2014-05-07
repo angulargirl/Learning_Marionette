25336074a0d52ad1718b1601852fe5cb6427139c
Claudia Frers <claudia.frers@gmail.com>
2014-05-07 10:03:08 +0200

    Refactor to use modules: call app and contact methods from index

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,8 +1,12 @@
 ContactManager.module("Entities",
     function (Entities, ContactManager, Backbone, Marionette, $, _) {
+        //private no attachment to module
         var alertPrivate = function () {
             alert('private function');
         };
+
+
+        //by attaching a method to the module, it becomes public
         Entities.alertPublic = function () {
             alertPrivate();
             alert('public function');
