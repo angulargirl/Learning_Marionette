3f4662b1303648171c8d82f3339104151376b193
Claudia Frers <claudia.frers@gmail.com>
2014-05-07 12:11:22 +0200

    use API in app to log contact list

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,5 +7,8 @@ ContactManager.addRegions({
 
 ContactManager.on("initialize:after", function () {
     //ContactManager.ContactsApp.List.Controller.listContacts();
+   // window.contacts = ContactManager.request("contact:entities");
+    console.log('Getting data from app:',ContactManager.request("contact:entities").toJSON);
+
 });
 
