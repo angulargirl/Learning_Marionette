9664ccece55266d833bf21db436b84c4dfaa9e14
Claudia Frers <claudia.frers@gmail.com>
2014-05-08 11:18:36 +0200

    add event handler to highleght clicked row and demo use of an event object

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -9,4 +9,32 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
     tagName: "ul",
     itemView: List.Contact
   });
+
+    // A Grid Row
+    List.Contact = Marionette.ItemView.extend({
+        tagName: "tr",
+        template: "#contact-list-item",
+        events: {//highlight the row that is clicked
+            "click": "highlightName"
+        },
+
+        highlightName: function(e){
+            // use the jquery event object
+            //Prevent a link from opening the URL & causing a refresh
+            e.preventDefault();
+
+            //this view instance's el element
+            this.$el.toggleClass("warning");
+        }
+
+    });
+
+    // A Grid Table
+    List.Contacts = Marionette.CompositeView.extend({
+        tagName: "table",
+        className: "table table-hover",
+        template: "#contact-list",
+        itemView: List.Contact,
+        itemViewContainer: "tbody"
+    });
 });
